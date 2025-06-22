a961871a4de821665a1491bdb1a0b3504d3a4146
abhishek0948 <abhishekkaratagi325@gmail.com>
2025-06-22 06:18:54 -0700

    logger service started

--- broker-service/cmd/api/handlers.go
+++ broker-service/cmd/api/handlers.go
@@ -69,10 +69,11 @@ func (app *Config) authenticate(w http.ResponseWriter, a AuthPayload) {
 	if response.StatusCode == http.StatusUnauthorized {
 		app.errorJSON(w, errors.New("invalid credentials"))
 		return
-	} else if response.StatusCode != http.StatusAccepted {
-		app.errorJSON(w, errors.New("error calling auth service"))
-		return
-	}
+	} 
+	// else if response.StatusCode != http.StatusAccepted {
+	// 	app.errorJSON(w, errors.New("error calling auth service"))
+	// 	return
+	// }
 
 	// create a variable we'll read response.Body into
 	var jsonFromService jsonResponse
